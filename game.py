fa222c50a593d719c51cd9085e246db4d8f7854a
kiganyamburu <mburukiganya@gmail.com>
2024-05-05 12:34:58 +0300

    comments fix

--- game.py
+++ game.py
@@ -1,12 +1,16 @@
 import time
+
 from player import HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
     def __init__(self):
+        # a single list to rep 3x3 board 
         self.board = [' ' for _ in range(9)]
+        # keep track of winner
         self.current_winner = None
         
     def print_board(self):
+        # getting the row
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('|' + '|'.join(row) +'|')
             
